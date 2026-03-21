ccded1d2826a8b3d01f8543cf82fe91758bc89ad
Ashutosh Panigrahi <ashutoshpanigrahi11@gmail.com>
2026-03-21 16:45:08 +0530

    Components are build

---
+++ frontend/src/ChatWindow.jsx
@@ -0,0 +1,13 @@
+import './ChatWindow.css'
+function ChatWindow(){
+    return (
+        <>
+            <div className='chatWindow'>
+                chatWindow
+            </div>
+        </>
+    )
+}
+
+
+export default ChatWindow;
---
+++ frontend/src/Sidebar.jsx
@@ -0,0 +1,10 @@
+import "./Sidebar.css";
+function Sidebar() {
+  return (
+    <>
+      <div className="sideBar">sidebar</div>
+    </>
+  );
+}
+
+export default Sidebar;
